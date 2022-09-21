d3e273fafe6d9941754ff051dece0874c9606af7
caio-silva <caio_ceac23@hotmail.com>
2022-09-21 15:56:12 -0300

    final-do-crud

--- clientes/views.py
+++ clientes/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from .models import Cliente
 from .forms import ClienteForm
 
@@ -16,6 +17,29 @@ def inserir_cliente(request):
         form = ClienteForm(request.POST)
         if form.is_valid():
             form.save()
+            return redirect('listar_clientes')  # REDIRECIONA PARA A LISTAGEM
     else:
         form = ClienteForm()
     return render(request, 'clientes/form_cliente.html', {'form': form})
+
+
+def listar_cliente_id(request, id):
+    cliente = Cliente.objects.get(id=id)
+    return render(request, 'clientes/listar_cliente_id.html', {'cliente': cliente})
+
+
+def editar_cliente(request, id):
+    cliente = Cliente.objects.get(id=id)
+    form = ClienteForm(request.POST or None, instance=cliente)
+    if form.is_valid():
+        form.save()
+        return redirect('listar_clientes')
+    return render(request, 'clientes/form_cliente.html', {'form': form})
+
+
+def remover_cliente(request, id):
+    cliente = Cliente.objects.get(id=id)
+    if request.method == 'POST':
+        cliente.delete()
+        return redirect('listar_clientes')
+    return render(request, 'clientes/confirma_exclusao.html', {'cliente': cliente})
