75fa1c45aa38c1c6c3d3a0416a796a7400eb4685
caio silva <caio_ceac23@hotmail.com>
2022-10-28 13:11:59 -0300

    update database

--- tw_clientes/settings.py
+++ tw_clientes/settings.py
@@ -76,14 +76,11 @@ WSGI_APPLICATION = 'tw_clientes.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'crud_web',
-        'USER': 'root',
-        'PASSWORD': '1234',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 # Password validation
